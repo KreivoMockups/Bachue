8810a55d7727ee256a55a385fd916f8008ca04d9
KreivoMockups <gerente.salvia@kreivo.co>
2026-03-23 17:36:02 -0500

    Eliminar data.js

--- data.js
+++
@@ -1,7 +0,0 @@
-/* data.js */
-// Extraemos el caso actual a evaluar desde el Registry
-const activeCase = MuiscaRegistry["CASE_GOV_001"];
-
-// Variables de estado global
-let currentStep = 0;
-let isSimulationRunning = false;
